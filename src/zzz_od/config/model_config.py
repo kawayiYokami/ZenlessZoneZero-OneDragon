a3dca4c785e01315640d347b4016ceff4eb1f2f8
DoctorReid <doctorreid2024@outlook.com>
2025-09-21 20:13:07 +0800

    feat: 迷失之地模型更新-202509 #1396

--- src/zzz_od/config/model_config.py
+++ src/zzz_od/config/model_config.py
@@ -15,8 +15,8 @@ _BACKUP_FLASH_CLASSIFIER = 'yolov8n-640-flash-20250906'
 _DEFAULT_HOLLOW_ZERO_EVENT = 'yolov8s-736-hollow-zero-event-0126'
 _BACKUP_HOLLOW_ZERO_EVENT = 'yolov8s-736-hollow-zero-event-1130'
 
-_DEFAULT_LOST_VOID_DET = 'yolov8n-736-lost-void-det-20250622'
-_BACKUP_LOST_VOID_DET = 'yolov8n-736-lost-void-det-20250612'
+_DEFAULT_LOST_VOID_DET = 'yolov8n-736-lost-void-det-20250921'
+_BACKUP_LOST_VOID_DET = 'yolov8n-736-lost-void-det-20250622'
 
 
 class ModelConfig(BasicModelConfig):
