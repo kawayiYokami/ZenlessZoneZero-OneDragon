a56f4271fb2219d34592791af78a87478668f3dd
DoctorReid <doctorreid2024@outlook.com>
2025-10-19 23:14:37 +0800

    fix: 尝试优化卦象集录

--- src/zzz_od/application/trigrams_collection/trigrams_collection_app.py
+++ src/zzz_od/application/trigrams_collection/trigrams_collection_app.py
@@ -65,15 +65,14 @@ class TrigramsCollectionApp(ZApplication):
         elif word == '滑动屏幕以获取卦象':
             start = Point(self.ctx.controller.standard_width // 2, self.ctx.controller.standard_height // 2)
             end = start + Point(-800, 0)
-            self.ctx.controller.drag_to(start=start, end=end, duration=0.2)
+            self.ctx.controller.drag_to(start=start, end=end, duration=2)  # 这里是越慢拖动越多
             return self.round_wait(status=word, wait=1)
         elif word == '确认':
             self.claim_reward = True
             self.ctx.controller.click(mrl.max.center)
             return self.round_wait(status=word, wait=1)
 
-        return self.round_by_click_area('卦象集录', '区域-开卦象',
-                                        success_wait=1)
+        return self.round_retry(status='未识别目标文本', wait=1)
 
     @node_from(from_name='获取卦象')
     @operation_node(name='结束后返回')
