0df0a5fb99fa3b4ad338ffce7d4bef8ebc0699d4
ShadowLemoon <119576779+ShadowLemoon@users.noreply.github.com>
2025-12-02 01:40:47 +0800

    chore: 优化体力计划的体力通知

--- src/zzz_od/application/charge_plan/charge_plan_app.py
+++ src/zzz_od/application/charge_plan/charge_plan_app.py
@@ -1,9 +1,9 @@
-from typing import ClassVar, Optional
+from typing import ClassVar
 
 from one_dragon.base.operation.application import application_const
 from one_dragon.base.operation.operation_edge import node_from
 from one_dragon.base.operation.operation_node import operation_node
-from one_dragon.base.operation.operation_notify import node_notify, NotifyTiming
+from one_dragon.base.operation.operation_notify import NotifyTiming, node_notify
 from one_dragon.base.operation.operation_round_result import OperationRoundResult
 from one_dragon.utils import cv2_utils, str_utils
 from zzz_od.application.charge_plan import charge_plan_const
@@ -15,10 +15,10 @@ from zzz_od.application.charge_plan.charge_plan_config import (
 from zzz_od.application.zzz_application import ZApplication
 from zzz_od.context.zzz_context import ZContext
 from zzz_od.operation.back_to_normal_world import BackToNormalWorld
+from zzz_od.operation.compendium.area_patrol import AreaPatrol
 from zzz_od.operation.compendium.combat_simulation import CombatSimulation
 from zzz_od.operation.compendium.expert_challenge import ExpertChallenge
 from zzz_od.operation.compendium.notorious_hunt import NotoriousHunt
-from zzz_od.operation.compendium.area_patrol import AreaPatrol
 from zzz_od.operation.compendium.tp_by_compendium import TransportByCompendium
 from zzz_od.operation.goto.goto_menu import GotoMenu
 from zzz_od.operation.restore_charge import RestoreCharge
@@ -45,8 +45,8 @@ class ChargePlanApp(ZApplication):
 
         self.charge_power: int = 0  # 剩余电量
         self.required_charge: int = 0  # 需要的电量
-        self.last_tried_plan: Optional[ChargePlanItem] = None
-        self.next_plan: Optional[ChargePlanItem] = None
+        self.last_tried_plan: ChargePlanItem | None = None
+        self.next_plan: ChargePlanItem | None = None
 
     @operation_node(name='开始体力计划', is_start_node=True)
     def start_charge_plan(self) -> OperationRoundResult:
@@ -64,7 +64,6 @@ class ChargePlanApp(ZApplication):
         return self.round_by_op_result(op.execute())
 
     @node_from(from_name='打开菜单')
-    @node_notify(when=NotifyTiming.CURRENT_SUCCESS, detail=True)
     @operation_node(name='识别电量')
     def check_charge_power(self) -> OperationRoundResult:
         # 不能在快捷手册里面识别电量 因为每个人的备用电量不一样
@@ -110,10 +109,7 @@ class ChargePlanApp(ZApplication):
                 else:
                     need_charge_power = int(candidate_plan.card_num) * 20
             elif candidate_plan.category_name == '区域巡防':
-                if self.config.use_coupon:
-                    need_charge_power = 0
-                else:
-                    need_charge_power = 60
+                need_charge_power = 0 if self.config.use_coupon else 60
             elif candidate_plan.category_name == '专业挑战室':
                 need_charge_power = 40
             elif candidate_plan.category_name == '恶名狩猎':
@@ -183,12 +179,12 @@ class ChargePlanApp(ZApplication):
         return self.round_by_op_result(op.execute())
 
     @node_from(from_name='实战模拟室', success=True)
-    @node_from(from_name='区域巡防', success=True)
-    @node_from(from_name='专业挑战室', success=True)
-    @node_from(from_name='恶名狩猎', success=True)
     @node_from(from_name='实战模拟室', success=False)
+    @node_from(from_name='区域巡防', success=True)
     @node_from(from_name='区域巡防', success=False)
+    @node_from(from_name='专业挑战室', success=True)
     @node_from(from_name='专业挑战室', success=False)
+    @node_from(from_name='恶名狩猎', success=True)
     @node_from(from_name='恶名狩猎', success=False)
     @operation_node(name='挑战完成')
     def challenge_complete(self) -> OperationRoundResult:
@@ -226,7 +222,9 @@ class ChargePlanApp(ZApplication):
     @node_from(from_name='电量不足', status=STATUS_ROUND_FINISHED)
     @node_from(from_name='查找并选择下一个可执行任务', status=STATUS_ROUND_FINISHED)
     @node_from(from_name='查找并选择下一个可执行任务', success=False)
+    @node_notify(when=NotifyTiming.CURRENT_DONE, detail=True)
     @operation_node(name='返回大世界')
     def back_to_world(self) -> OperationRoundResult:
         op = BackToNormalWorld(self.ctx)
-        return self.round_by_op_result(op.execute())
+        op_result = op.execute()
+        return self.round_by_op_result(op_result, status=f'剩余电量 {self.charge_power}')
