a1f3d1ad13e2318979fc5d98803e5f970e6692ea
wusaqi <2181802336@qq.com>
2026-01-01 23:07:02 +0800

    feat: 适配叶瞬光和简的皮肤 (#1827)
    * feat: 添加新皮肤资源和更新代理数据
    
    Signed-off-by: theresa10086 <theresa10086@github.com>
    
    * 修正名字与游戏内一致
    
    ---------
    
    Signed-off-by: theresa10086 <theresa10086@github.com>
    Co-authored-by: theresa10086 <799617009@qq.com>

--- src/zzz_od/game_data/agent.py
+++ src/zzz_od/game_data/agent.py
@@ -254,7 +254,7 @@ class AgentEnum(Enum):
                      state_list=[AgentStateDef('青衣-电压', AgentStateCheckWay.BACKGROUND_GRAY_RANGE_LENGTH,
                                                template_id='qingyi', lower_color=0, upper_color=70)])
 
-    JANE_DOE = Agent('jane_doe', '简', RareTypeEnum.S, AgentTypeEnum.ANOMALY, DmgTypeEnum.PHYSICAL, ['jane_doe'],
+    JANE_DOE = Agent('jane_doe', '简', RareTypeEnum.S, AgentTypeEnum.ANOMALY, DmgTypeEnum.PHYSICAL, ['jane_doe', 'jane_doe_nocturne_of_light'],
                      state_list=[AgentStateDef('简-萨霍夫跳', AgentStateCheckWay.COLOR_RANGE_EXIST,
                                                template_id='jane_attack', lower_color=(100, 20, 20), upper_color=(255, 255, 255), connect_cnt=20),
                                  AgentStateDef('简-狂热心流', AgentStateCheckWay.FOREGROUND_COLOR_RANGE_LENGTH,
@@ -420,7 +420,7 @@ class AgentEnum(Enum):
                                    max_length=100)
                  ])
 
-    YESHUNGUANG = Agent('yeshunguang', '叶瞬光', RareTypeEnum.S, AgentTypeEnum.ATTACK, DmgTypeEnum.PHYSICAL, ['yeshunguang'],
+    YESHUNGUANG = Agent('yeshunguang', '叶瞬光', RareTypeEnum.S, AgentTypeEnum.ATTACK, DmgTypeEnum.PHYSICAL, ['yeshunguang', 'yeshunguang_touch_of_dawnlight'],
                        state_list=[
                            AgentStateDef('叶瞬光-常态', AgentStateCheckWay.TEMPLATE_FOUND,
                                          template_id='yeshunguang_normal', min_value_trigger_state=0,
