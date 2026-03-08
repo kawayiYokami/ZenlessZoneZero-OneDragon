0c423dad4fcda7a55e5d0e0ef017d9b8da780321
kawayiYokami <289104862@qq.com>
2026-03-08 23:33:34 +0800

    fix: 禁用仓库扫描翻译自动更新

--- src/zzz_od/application/inventory_scan/translation/__init__.py
+++ src/zzz_od/application/inventory_scan/translation/__init__.py
@@ -1,4 +1,3 @@
 from zzz_od.application.inventory_scan.translation.translation_service import TranslationService
-from zzz_od.application.inventory_scan.translation.translation_updater import TranslationUpdater
 
-__all__ = ['TranslationService', 'TranslationUpdater']
+__all__ = ['TranslationService']
