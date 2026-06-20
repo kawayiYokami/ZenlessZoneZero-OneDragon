e21672e070ed27cdc86edb1989687b295498c605
Josh Cai <joshcai_mdwechat@163.com>
2026-06-20 09:42:33 +0800

    占卜加速 (#2326)
    * 占卜加速
    
    * 占卜再加速

--- src/zzz_od/application/trigrams_collection/trigrams_collection_app.py
+++ src/zzz_od/application/trigrams_collection/trigrams_collection_app.py
@@ -1,6 +1,7 @@
 import time
 
 from one_dragon.base.geometry.point import Point
+from one_dragon.base.geometry.rectangle import Rect
 from one_dragon.base.matcher.ocr import ocr_utils
 from one_dragon.base.operation.operation_edge import node_from
 from one_dragon.base.operation.operation_node import operation_node
@@ -48,7 +49,14 @@ class TrigramsCollectionApp(ZApplication):
     @node_notify(when=NotifyTiming.CURRENT_DONE)
     @operation_node(name='获取卦象', node_max_retry_times=10)
     def get_trigram(self) -> OperationRoundResult:
-        ocr_result_map = self.ctx.ocr.run_ocr(self.last_screenshot)
+        # ocr加速: 只识别下半部分屏幕
+        offset: Point = Point(0, self.ctx.controller.standard_height // 2)
+        ocr_result_map = self.ctx.ocr.crop_and_run_ocr(
+            self.last_screenshot,
+            Rect(0,
+                 self.ctx.controller.standard_height // 2,
+                 self.ctx.controller.standard_width,
+                 self.ctx.controller.standard_height))
 
         target_word_list: list[str] = [
             '卦象集录',  # 外层还没开卦象的时候
@@ -60,16 +68,19 @@ class TrigramsCollectionApp(ZApplication):
             if self.claim_reward:
                 return self.round_success(status=word)
             else:
-                result = self.round_by_click_area('卦象集录', '区域-获取卦象')
+                self.round_by_click_area('卦象集录', '区域-获取卦象')
                 return self.round_wait(status=word, wait=1)
         elif word == '滑动屏幕以获取卦象':
-            start = Point(self.ctx.controller.standard_width // 2, self.ctx.controller.standard_height // 2)
-            end = start + Point(-800, 0)
-            self.ctx.controller.drag_to(start=start, end=end, duration=2)  # 这里是越慢拖动越多
-            return self.round_wait(status=word, wait=1)
+            start = Point(self.ctx.controller.standard_width - 100, 100)
+            end = Point(100, self.ctx.controller.standard_height - 100)
+            self.ctx.controller.drag_to(start=start, end=end, duration=1)  # 这里是越慢拖动越多
+            self.ctx.controller.drag_to(start=end, end=start, duration=1)
+            return self.round_wait(status=word)
         elif word == '确认':
             self.claim_reward = True
-            self.ctx.controller.click(mrl.max.center)
+            area: Point = mrl.max.center
+            area = area.__add__(offset)
+            self.ctx.controller.click(area)
             return self.round_wait(status=word, wait=1)
 
         return self.round_retry(status='未识别目标文本', wait=1)
